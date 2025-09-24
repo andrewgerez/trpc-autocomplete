0ec9404cde24d39981e11039be5b506b058f4721
Andrew <andrew.gerez@watch.tv.br>
2025-09-24 10:44:13 -0300

    feat(240925): trpc autocomplete project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Geist } from "next/font/google";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const geist = Geist({
   subsets: ["latin"],
@@ -12,6 +13,11 @@ const geist = Geist({
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <div className={geist.className}>
+      <Head>
+        <title>City Finder</title>
+        <meta name="description" content="Search and explore cities from around the world" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
